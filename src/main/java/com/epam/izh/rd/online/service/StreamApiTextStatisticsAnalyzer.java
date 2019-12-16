b948f6035a040ff395b06e1a10caa06772c5212c
Egor <egot.dubovczcev@yandex.ru>
2019-12-16 09:00:28 +0400

    Finished work

--- src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
+++ src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
@@ -2,12 +2,10 @@ package com.epam.izh.rd.online.service;
 
 import com.epam.izh.rd.online.helper.Direction;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import static java.util.Collections.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Данный класс обязан использовать StreamApi из функционала Java 8. Функциональность должна быть идентична
@@ -16,36 +14,62 @@ import static java.util.Collections.*;
 public class StreamApiTextStatisticsAnalyzer implements TextStatisticsAnalyzer {
     @Override
     public int countSumLengthOfWords(String text) {
-        return 0;
+        List<String> words = getWords(text);
+        return words.stream().reduce(0, (sum, s) -> sum += s.length(), Integer::sum);
     }
+
 
     @Override
     public int countNumberOfWords(String text) {
-        return 0;
+        List<String> words = getWords(text);
+        return words.stream().reduce(0, (sum, s) -> sum += 1, Integer::sum);
+
     }
 
     @Override
     public int countNumberOfUniqueWords(String text) {
-        return 0;
+        List<String> words = getWords(text);
+        List<String> repeatedWords = new ArrayList<>();
+        return (int) words.stream().filter((o1) -> !repeatedWords.contains(o1)).map(repeatedWords::add).count();
     }
 
     @Override
     public List<String> getWords(String text) {
-        return emptyList();
+        Pattern pattern = Pattern.compile("[\\W+]");
+        Matcher matcher = pattern.matcher(text);
+
+        String textVer2 = matcher.replaceAll("1");
+        String[] wordsArray = textVer2.split("1+");
+        return Arrays.stream(wordsArray).collect(Collectors.toList());
+
+
     }
 
     @Override
     public Set<String> getUniqueWords(String text) {
-        return emptySet();
+        List<String> words = getWords(text);
+        return words.stream().collect(Collectors.toSet());
     }
 
     @Override
     public Map<String, Integer> countNumberOfWordsRepetitions(String text) {
-        return emptyMap();
+        List<String> words = getWords(text);
+        Set<String> wordSet = new HashSet<>(words);
+        Map<String, Integer> countedWords = new HashMap<>();
+        wordSet.stream().forEach((s) -> countedWords.put(s, countRepeats(s, words)));
+        return countedWords;
+    }
+
+    private int countRepeats(String word, List<String> list) {
+        return (int) list.stream().filter(s -> s.equals(word)).count();
     }
 
     @Override
     public List<String> sortWordsByLength(String text, Direction direction) {
-        return emptyList();
+        List<String> words = getWords(text);
+        Comparator<String> comparator = Comparator.comparing(String::length);
+        comparator = direction == Direction.ASC ? comparator : comparator.reversed();
+        return words.stream().sorted(comparator).collect(Collectors.toList());
+
     }
 }
