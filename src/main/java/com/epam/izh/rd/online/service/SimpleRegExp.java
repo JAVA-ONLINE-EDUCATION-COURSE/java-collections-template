925d35b8c4f216efe77495b64d4d4633a93bbc4a
Kirill <kirill_nn@inbox.ru>
2020-01-04 19:50:26 +0300

    Остался самый веселый метод

--- src/main/java/com/epam/izh/rd/online/service/SimpleRegExp.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleRegExp.java
@@ -1,6 +1,7 @@
 package com.epam.izh.rd.online.service;
 
 class SimpleRegExp {
+    //Может зря, но ввел класс, для RegExp
     String REG_EXP_WORD() {
         return "[^a-zA-Zа-яА-Я]+";
     }
--- src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
+++ src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
@@ -2,41 +2,61 @@ package com.epam.izh.rd.online.service;
 
 import com.epam.izh.rd.online.helper.Direction;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-import static java.util.Collections.*;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.emptyMap;
 
 /**
  * Данный класс обязан использовать StreamApi из функционала Java 8. Функциональность должна быть идентична
  * {@link SimpleTextStatisticsAnalyzer}.
  */
 public class StreamApiTextStatisticsAnalyzer implements TextStatisticsAnalyzer {
+    //Надеюсь, я правильно понял, чего от меня хотят в этом задании, просто какие-то Stream'ы получаются
+    // слишком избыточными (неуместными), не знаю, как еще сказать.
+
+    private SimpleRegExp simpleRegExp = new SimpleRegExp();
+
     @Override
     public int countSumLengthOfWords(String text) {
-        return 0;
+        return getWords(text).stream().mapToInt(String::length).sum();
     }
 
     @Override
     public int countNumberOfWords(String text) {
-        return 0;
+        return (int) getWords(text)
+                .stream()
+                .count();
     }
 
     @Override
     public int countNumberOfUniqueWords(String text) {
-        return 0;
+        return (int) getWords(text)
+                .stream()
+                .distinct()
+                .count();
     }
 
     @Override
     public List<String> getWords(String text) {
-        return emptyList();
+        if (text == null) {
+            return emptyList();
+        }
+
+        return Arrays.stream(text.split(simpleRegExp.REG_EXP_WORD()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public Set<String> getUniqueWords(String text) {
-        return emptySet();
+        //Вот тут IDEA вабще говорит, давай-ка HashSet
+        return getWords(text)
+                .stream()
+                .collect(Collectors.toSet());
     }
 
     @Override
@@ -46,6 +66,16 @@ public class StreamApiTextStatisticsAnalyzer implements TextStatisticsAnalyzer {
 
     @Override
     public List<String> sortWordsByLength(String text, Direction direction) {
-        return emptyList();
+        if (direction == Direction.ASC) {
+            return getWords(text)
+                    .stream()
+                    .sorted(new SimpleStringLengthComparator())
+                    .collect(Collectors.toList());
+        } else {
+            return getWords(text)
+                    .stream()
+                    .sorted(new SimpleStringLengthComparator().reversed())
+                    .collect(Collectors.toList());
+        }
     }
 }
