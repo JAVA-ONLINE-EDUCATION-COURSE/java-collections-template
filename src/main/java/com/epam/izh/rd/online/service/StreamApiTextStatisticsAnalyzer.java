96e06b5f409f3e29e5f0038665fbe3ee58fd4ea5
Yurii Erofeevskii <Borlok@bk.ru>
2020-07-05 14:48:44 +0300

    Done

--- src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
+++ src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
@@ -2,10 +2,8 @@ package com.epam.izh.rd.online.service;
 
 import com.epam.izh.rd.online.helper.Direction;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.*;
 
@@ -16,36 +14,40 @@ import static java.util.Collections.*;
 public class StreamApiTextStatisticsAnalyzer implements TextStatisticsAnalyzer {
     @Override
     public int countSumLengthOfWords(String text) {
-        return 0;
+        return Arrays.stream(text.split("\\s*\\W\\s*")).filter(x -> !x.equals("")).mapToInt(String::length).sum();
     }
 
     @Override
     public int countNumberOfWords(String text) {
-        return 0;
+        return (int) Arrays.stream(text.split("\\s*\\W\\s*")).filter(x -> !x.equals("")).count();
     }
 
     @Override
     public int countNumberOfUniqueWords(String text) {
-        return 0;
+        return (int) Arrays.stream(text.split("\\s*\\W\\s*")).filter(x -> !x.equals("")).distinct().count();
     }
 
     @Override
     public List<String> getWords(String text) {
-        return emptyList();
+
+        return Arrays.stream(text.split("\\s*\\W\\s*")).filter(x -> !x.equals("")).collect(Collectors.toList());
     }
 
     @Override
     public Set<String> getUniqueWords(String text) {
-        return emptySet();
+
+        return getWords(text).stream().collect(Collectors.toSet());
     }
 
     @Override
     public Map<String, Integer> countNumberOfWordsRepetitions(String text) {
-        return emptyMap();
+        return getWords(text).stream().collect(Collectors.toMap(key -> key, value -> 1, Integer::sum));
     }
 
     @Override
     public List<String> sortWordsByLength(String text, Direction direction) {
-        return emptyList();
+        return direction.name().contains("ASC") ?
+                getWords(text).stream().sorted(Comparator.comparing(String::length)).collect(Collectors.toList()) :
+                getWords(text).stream().sorted(Comparator.comparing(String::length).reversed()).collect(Collectors.toList());
     }
 }
